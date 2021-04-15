038e17fbfd293dcb424c41ebaa91134ea69fb115
MCardenas38 <mcarden38@gmail.com>
2021-04-15 17:39:43 -0400

    BottomViewNav

--- app/src/main/java/com/example/memeit/MainActivity.java
+++ app/src/main/java/com/example/memeit/MainActivity.java
@@ -1,22 +1,30 @@
 package com.example.memeit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
+    private BottomNavigationView bottomNav;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.button);
+        bottomNav= findViewById(R.id.botton_navigation);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,5 +34,27 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.postings:
+                        Toast.makeText(MainActivity.this,"Posts",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.settings:
+                        Toast.makeText(MainActivity.this,"Settings",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.saved:
+                        Toast.makeText(MainActivity.this,"Saved",Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
+
     }
 }
